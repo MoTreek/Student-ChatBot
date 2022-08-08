c6859beb8a380d76315a699cc498bb9e59605a9b
Mohamed Treek <Mohamedtreek0@gmail.com>
2022-08-08 11:51:05 -0400

    First Commit

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -8,6 +8,10 @@ export default {
 
   register(user) {
     return axios.post('/register', user)
+  },
+
+  getUser(user) {
+    return axios.get('/login', user)
   }
 
 }
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>Home </h1>
+    <p>Welcome {{this.$store.state.user.username}}</p>
   </div>
 </template>
 
