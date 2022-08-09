d36559f71021a1a2b14f43682a105359b96b4aac
Mohamed Treek <Mohamedtreek0@gmail.com>
2022-08-09 10:25:06 -0400

    Split

---
+++ vue/src/components/ChatBox.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="Chat-box">
+        <div class="Chat-box-response" ref="chatbox">
+
+        </div>
+
+        <div class="Chat-input">
+            <input type="text"
+            v-model="message"
+            @keyup.enter="sendMessage" />
+        </div>
+
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ChatBox",
+    data()  {
+        return {
+        message: '',
+        messages: []
+        }
+    },
+    
+    methods: {
+        sendMessage() {
+            this.messages.push({
+                text:this.message,
+                author: 'client'
+            })
+
+            this.message = '',
+            this.$nextTick(() => {
+                this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+            })
+
+            //Connect and search database
+        }
+    }
+}
+</script>
+
+<style>
+.Chat-box,
+.Chat-box-reponse{
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+}
+
+.Chat-box-reponse{
+    overflow: scroll;
+}
+
+.Chat-bot-response{
+    padding-left: 0;
+
+    
+}
+
+.Chat-box{
+    border: 1px solid red;
+    width: 50vw;
+    border-radius: 4px;
+
+}
+
+</style>
